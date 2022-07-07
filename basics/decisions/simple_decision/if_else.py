1048fce0c0d2b44ed75f99ee35542323bfa8279a
CiobanuDumitru <>
2022-07-07 14:35:35 +0000

    Completed week 2 task 2

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,8 @@
+activity = input("Activity: ")
+
+if activity.lower() == 'calculate':
+  print("Performing calculations...")
+else:
+  print("Performing activity")
+
+print ("Activity completed!")
--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 print("Hello World!")
 print("Dumitru is my name") 
-
-book_type = input("What type of book is this? ")
-if book_type == 'adventure':
-  print("I like adventure books!")
-print("Finished reading book!")
