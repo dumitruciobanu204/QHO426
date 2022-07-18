669dcc0f60c6a05f210ccb018c784ea901ee3b19
CiobanuDumitru <>
2022-07-18 13:53:57 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,9 @@
+def identify():
+  print("What lies before us?")
+  n = input()
+  if n.lower() == 'a large boulder':
+    print("It's time to run!")
+  else:
+    print("We will be fine.")
+
+identify()
